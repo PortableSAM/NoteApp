3af9772f1b4c9b18a0d3f74e7947273319ec27c4
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2020-01-18 20:04:58 +0900

    firebase 추가 / NoteApp, Note 작성 완료

--- src/Components/Note.js
+++ src/Components/Note.js
@@ -1,9 +1,26 @@
 import React from "react";
+import firebase from "./Fire/Fire_Config";
 
-function Note() {
+function Note({ id, title, task, place, date }) {
+  const onDelete = () => {
+    const db = firebase.firestore();
+    const dbRef = db.collection("noteapp").doc(id);
+    dbRef.delete();
+  };
+
   return (
-    <div>
-      <h3>Note warpper</h3>
+    <div className="note-container">
+      <div className="note-item">
+        <h3>Title: {title}</h3>
+        <h3>To-do: {task}</h3>
+        <h3>Place: {place}</h3>
+        <h3>Date: {date}</h3>
+      </div>
+      <div className="del-btn">
+        <button className="btn btn-outline-danger" onClick={onDelete}>
+          Delete
+        </button>
+      </div>
     </div>
   );
 }
--- src/Components/NoteApp.js
+++ src/Components/NoteApp.js
@@ -3,17 +3,47 @@ import { Container, Row, Col } from "reactstrap";
 import Note from "./Note";
 import NoteForm from "./NoteForm";
 import "./NoteApp.css";
+import firebase from "./Fire/Fire_Config";
 
 function NoteApp() {
+  const [note, setNote] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchData = () => {
+      const db = firebase.firestore();
+      //Firestore Collection and orderBy Set
+      const dbRef = db.collection("noteapp").orderBy("createAt", "desc");
+      //FireStore RealTime listener
+      dbRef.onSnapshot(snapshot => {
+        const newNote = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+        setNote(newNote);
+      });
+    };
+    return fetchData();
+  }, []);
+
+  console.log(note);
   return (
     <Container>
       <Row>
-        <Col lg={8} md={"auto"} sm={"auato"}>
+        <Col lg={12} md={"auto"} sm={"auato"}>
           <h2>Note App</h2>
         </Col>
       </Row>
       <div className="warpper-container">
-        <Note />
+        {note.map((note, index) => (
+          <Note
+            id={note.id}
+            key={index}
+            title={note.title}
+            task={note.task}
+            place={note.place}
+            date={new Date(note.createAt.seconds * 1000).toDateString("ko")}
+          />
+        ))}
       </div>
       <div className="App-footer">
         <NoteForm />
